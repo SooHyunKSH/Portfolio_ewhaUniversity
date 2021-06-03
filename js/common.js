5bd02f3de2cbef56a85733f8f8d407238fda82be
SooHyunKSH <hyun29075691@gmail.com>
2021-06-03 18:11:42 +0900

    🎈메인페이지 추가

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
 $(function(){
-  
+  /* 상단으로 이동버튼 */
+  $(window).scroll(function(){
+    console.log($(this).scrollTop());
+    if( $(this).scrollTop() == 0 ){
+      $(".go_to_top").removeClass("on");
+    }else{
+      $(".go_to_top").addClass("on");
+    };
+  });
 })
