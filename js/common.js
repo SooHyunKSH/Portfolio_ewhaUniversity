cfeff004d5cd0b10dd68da416251a46199815947
SooHyunKSH <82920745+SooHyunKSH@users.noreply.github.com>
2021-06-04 00:26:23 +0900

    🖥메인페이지 드롭다운 서브메뉴 추가

--- js/common.js
+++ js/common.js
@@ -4,8 +4,29 @@ $(function(){
     console.log($(this).scrollTop());
     if( $(this).scrollTop() == 0 ){
       $(".go_to_top").removeClass("on");
+      $(".header_top_wrap").removeClass("on");
+      $(".header_md_wrap").removeClass("on");
+      $(".header_bottom_wrap").removeClass("on");
     }else{
       $(".go_to_top").addClass("on");
+      $(".header_top_wrap").addClass("on");
+      $(".header_md_wrap").addClass("on");
+      $(".header_bottom_wrap").addClass("on");
     };
   });
+
+  /* sub_gnb 드롭다운메뉴 */
+  // sub_gnb li 너비 계산
+  $(".sub_gnb > ul > li").each(function(){
+    let subgnbLi = $(".sub_gnb > ul > li").length;
+    $(this).css("width", "calc(100% / "+subgnbLi+")");
+  })
+  // 마우스오버시 sub_gnb 나타나기
+  $(".gnb li").mouseenter(function(){
+    $(this).siblings().children(".sub_gnb").removeClass("on");
+    $(this).children(".sub_gnb").addClass("on");
+  }).mouseleave(function(){
+    $(this).children(".sub_gnb").removeClass("on");
+  });
+
 })
