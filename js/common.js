9994e3f0f918e1521f23809c092ad902fdf2c27f
SooHyunKSH <82920745+SooHyunKSH@users.noreply.github.com>
2021-06-09 23:31:14 +0900

    💜반응형 작업 완료

--- js/common.js
+++ js/common.js
@@ -33,12 +33,36 @@ $(function(){
     $(this).children(".sub_gnb").removeClass("on");
   });
 
+  /* 모바일 sub menu 나타나기 */
+  $(".mobile_gnb_btn").click(function(){
+    $(".m_slide_menu").addClass("on");
+  });
+  $(".m_close").click(function(){
+    $(".m_slide_menu").removeClass("on");
+  });
+
   /* subpage 상단 tab menu 나타나기 */
   $(".tab_box > ul > li").mouseenter(function(){
     $(this).siblings().children(".sub_tab").removeClass("on");
     $(this).children(".sub_tab").addClass("on");
   }).mouseleave(function(){
     $(this).children(".sub_tab").removeClass("on");
+  });
+  // m_gnb 클릭 시 m_sub_gnb 나타나기
+  $(".m_gnb li").click(function(){
+    $(this).each(function(){
+      $('.m_sub_gnb').removeClass("on")
+      $('.m_gnb li a').removeClass("click");
+      $(this).children('.m_sub_gnb').addClass("on")
+      $(this).children('a').addClass("click");
+    })
+  })
+  // sub_gnb 아코디언
+  $(".m_sub_gnb > ul > li > a").on("click",function(){
+    if( $("+ul",this).css("display") == "none"){
+      $(".m_sub_gnb02").slideUp();
+      $("+ul",this).slideDown();
+    }
   });
 
       /* click list li 클릭 시 display0$ 나오기 */
@@ -69,13 +93,25 @@ $(function(){
       });
 
           /* select list 클릭 시 display_box 나타내기 */
-    $("#select_tab").on("change", function (i) {
+    $("#select_tab").on("change", function () {
       $(".display_wrap .display_box").each(function () {
           if ($("#select_tab").val() == $(this).index() + 1) {
               $(".display_wrap .display_box").css("display", "none");
               $(this).css("display", "block");
           }
-      })
-      console.log($('#select_tab').val())
+      });
+      /* 선택된 option 텍스트 label테그에 넣기 */
+    $(".select_list label").text($("#select_tab option:selected").text());
   });
+
+  $("#select_tab02").on("change", function (i) {
+    $(".display_wrap .display_box_type02").each(function () {
+        if ($("#select_tab02").val() == $(this).index() + 1) {
+            $(".display_wrap .display_box_type02").css("display", "none");
+            $(this).css("display", "block");
+        }
+    });
+    /* 선택된 option 텍스트 label테그에 넣기 */
+  $(".select_list02 label").text($("#select_tab02 option:selected").text());
+});
 })
